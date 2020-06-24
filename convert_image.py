64dc07b1f767d2c6589ae5a7450854d413024e84
Howard Huang <howard@pop-os.localdomain>
2020-06-24 22:05:49 +0800

    updated converting code

--- convert_image.py
+++ convert_image.py
@@ -13,30 +13,6 @@ path_originals = '/home/howard/Homework/Algorithm/Final_Project/handwrite_detect
 if not os.path.exists(path):
     os.makedirs(path)
 
-def compress_image(image, infile):
-    size = 28, 28
-    width = 28
-    height = 28
-    listing = os.listdir(path_originals)
-
-    name = infile.split('.')
-    first_name = path+'/'+name[0] + '.jpg'
-    if image.size[0] > width and image.size[1] > height:
-        image.thumbnail(size, Image.ANTIALIAS)
-        image.save(first_name, quality=85)
-    elif image.size[0] > width:
-        wpercent = (width/float(image.size[0]))
-        height = int((float(image.size[1])*float(wpercent)))
-        image = image.resize((width,height), PIL.Image.ANTIALIAS)
-        image.save(first_name,quality=85)
-    elif image.size[1] > height:
-        wpercent = (height/float(image.size[1]))
-        width = int((float(image.size[0])*float(wpercent)))
-        image = image.resize((width,height), PIL.Image.ANTIALIAS)
-        image.save(first_name, quality=85)
-    else:
-        image.save(first_name, quality=85)
-
 
 def processImage():
     listing = os.listdir(path_originals)
@@ -45,31 +21,7 @@ def processImage():
         name = infile.split('.')
         first_name = path+'/'+name[0] + '.jpg'
 
-        if img.format == "JPEG":
-            image = img.convert('RGB')
-            compress_image(image, infile)
-            img.close()
-
-        elif img.format == "GIF":
-            i = img.convert("RGBA")
-            bg = Image.new("RGBA", i.size)
-            image = Image.composite(i, bg, i)
-            compress_image(image, infile)
-            img.close()
-
-        elif img.format == "PNG":
-            try:
-                image = Image.new("RGB", img.size, (255,255,255))
-                image.paste(img,img)
-                compress_image(image, infile)
-            except ValueError:
-                image = img.convert('RGB')
-                compress_image(image, infile)
-            img.close()
-
-        elif img.format == "BMP":
-            image = img.convert('RGB')
-            compress_image(image, infile)
-            img.close()
+        image = img.convert('RGB')
+        img.close()
 
 processImage()
